299e8962566f87cbeda64522acbcfca6ece298cc
Eric Mueller <nevinera@gmail.com>
2023-05-03 08:25:32 -0400

    add missing spec bodies

--- spec/quiet_quality/annotators_spec.rb
+++ spec/quiet_quality/annotators_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe QuietQuality::Annotators do
   describe ".annotate!" do
-    let(:annotator) { :github_stdout }
     let(:limit) { nil }
     let(:messages) { generate_messages(10) }
     subject(:annotate!) { described_class.annotate!(annotator: annotator, messages: messages, limit: limit) }
@@ -13,13 +12,37 @@ RSpec.describe QuietQuality::Annotators do
       end
     end
 
-    context "when the limit isn't set" do
-    end
+    context "for the :github_stdout annotator" do
+      let(:annotator) { :github_stdout }
+      let(:fake_annotator) { instance_double(QuietQuality::Annotators::GithubStdout, annotate!: nil) }
+      before { allow(QuietQuality::Annotators::GithubStdout).to receive(:new).and_return(fake_annotator) }
 
-    context "when the limit is larger than the message count" do
-    end
+      context "when the limit isn't set" do
+        let(:limit) { nil }
 
-    context "when the limit is smaller than the message count" do
+        it "invokes the annotator correctly" do
+          annotate!
+          expect(fake_annotator).to have_received(:annotate!).with(messages)
+        end
+      end
+
+      context "when the limit is larger than the message count" do
+        let(:limit) { 15 }
+
+        it "invokes the annotator correctly" do
+          annotate!
+          expect(fake_annotator).to have_received(:annotate!).with(messages)
+        end
+      end
+
+      context "when the limit is smaller than the message count" do
+        let(:limit) { 3 }
+
+        it "invokes the annotator correctly" do
+          annotate!
+          expect(fake_annotator).to have_received(:annotate!).with(messages.first(3))
+        end
+      end
     end
   end
 end
