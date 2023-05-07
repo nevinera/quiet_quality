1b8c7e83ca61ddf744c4b0ebbe80bfec7a117f84
Eric Mueller <nevinera@gmail.com>
2023-05-07 10:41:39 -0400

    add the Cli::OptionParser
    this _parses_ the cli arguments, but doesn't turn them into Options objects.
    the cascading behavior needs us to parse all the arguments before constructing
    the options, which makes this a two-step process, and simpler to do with two
    classes (one would have been unwieldy anyway)

---
+++ lib/quiet_quality/cli/option_parser.rb
@@ -0,0 +1,89 @@
+require "optparse"
+
+module QuietQuality
+  module Cli
+    class OptionParser
+      attr_reader :options, :tool_options, :output
+
+      def initialize(args)
+        @args = args
+        @options = {}
+        @tool_options = {}
+        @output = nil
+      end
+
+      def parse!
+        parser.parse!(@args)
+        [positional, options, tool_options]
+      end
+
+      def positional
+        @args
+      end
+
+      private
+
+      def parser
+        ::OptionParser.new do |parser|
+          setup_banner(parser)
+          setup_help_output(parser)
+          setup_annotation_options(parser)
+          setup_file_target_options(parser)
+          setup_filter_messages_options(parser)
+        end
+      end
+
+      def setup_banner(parser)
+        parser.banner = "Usage: qq [TOOLS] [GLOBAL_OPTIONS] [TOOL_OPTIONS]"
+      end
+
+      def setup_help_output(parser)
+        parser.on("-h", "--help", "Prints this help") do
+          @output = parser.to_s
+          @options[:exit_immediately] = true
+        end
+      end
+
+      def setup_annotation_options(parser)
+        parser.on("-A", "--annotate ANNOTATOR", "Annotate with this annotator") do |name|
+          fail(UsageError, "Annotator not recognized: #{name}") unless Annotators::ANNOTATOR_TYPES.include?(name.to_sym)
+          @options[:annotator] = name.to_sym
+        end
+
+        # shortcut option
+        parser.on("-G", "--annotate-github-stdout", "Annotate with GitHub Workflow commands") do
+          @options[:annotator] = :github_stdout
+        end
+      end
+
+      def read_tool_or_global_option(name, tool, value)
+        if tool
+          @tool_options[tool.to_sym] ||= {}
+          @tool_options[tool.to_sym][name] = value
+        else
+          @options[name] = value
+        end
+      end
+
+      def setup_file_target_options(parser)
+        parser.on("-a", "--all-files [tool]", "Use the tool(s) on all files") do |tool|
+          read_tool_or_global_option(:all_files, tool, true)
+        end
+
+        parser.on("-c", "--changed-files [tool]", "Use the tool(s) only on changed files") do |tool|
+          read_tool_or_global_option(:all_files, tool, false)
+        end
+      end
+
+      def setup_filter_messages_options(parser)
+        parser.on("-f", "--filter-messages [tool]", "Filter messages from tool(s) based on changed lines") do |tool|
+          read_tool_or_global_option(:filter_messages, tool, true)
+        end
+
+        parser.on("-u", "--unfiltered [tool]", "Don't filter messages from tool(s)") do |tool|
+          read_tool_or_global_option(:filter_messages, tool, false)
+        end
+      end
+    end
+  end
+end
---
+++ spec/quiet_quality/cli/option_parser_spec.rb
@@ -0,0 +1,103 @@
+RSpec.describe QuietQuality::Cli::OptionParser do
+  let(:parser) { described_class.new(args) }
+  let(:parsed) { parser.parse! }
+  let(:parsed_positionals) { parsed[0] }
+  let(:parsed_options) { parsed[1] }
+  let(:parsed_tool_options) { parsed[2] }
+
+  def expect_global_options(**opts)
+    opts.each_pair { |name, value| expect(parsed_options[name]).to eq(value) }
+  end
+
+  def expect_tool_options(tool, opts)
+    tool_options = parsed_tool_options[tool] || {}
+    opts.each_pair { |name, value| expect(tool_options[name]).to eq(value) }
+  end
+
+  def self.expect_options(desc, arguments, global: {}, **tools)
+    context("with #{desc}") do
+      let(:args) { arguments }
+
+      it "sets the expected options" do
+        expect_global_options(**global) if global.any?
+        tools.each_pair do |tool_name, tool_options|
+          expect_tool_options(tool_name, tool_options)
+        end
+      end
+    end
+  end
+
+  def self.expect_usage_error(desc, arguments, matcher)
+    context "with #{desc}" do
+      let(:args) { arguments }
+
+      it "raises a UsageError" do
+        expect { parser.parse! }.to raise_error(QuietQuality::Cli::UsageError, matcher)
+      end
+    end
+  end
+
+  describe "help option" do
+    let(:args) { ["--help"] }
+
+    it "sets exit_immediately to true" do
+      expect(parsed_options[:exit_immediately]).to be_truthy
+    end
+
+    it "sets the output as expected" do
+      parser.parse!
+      expect(parser.output).to eq(<<~HELP_OUTPUT)
+        Usage: qq [TOOLS] [GLOBAL_OPTIONS] [TOOL_OPTIONS]
+            -h, --help                       Prints this help
+            -A, --annotate ANNOTATOR         Annotate with this annotator
+            -G, --annotate-github-stdout     Annotate with GitHub Workflow commands
+            -a, --all-files [tool]           Use the tool(s) on all files
+            -c, --changed-files [tool]       Use the tool(s) only on changed files
+            -f, --filter-messages [tool]     Filter messages from tool(s) based on changed lines
+            -u, --unfiltered [tool]          Don't filter messages from tool(s)
+      HELP_OUTPUT
+    end
+  end
+
+  describe "annotation options" do
+    subject(:annotation_option) { parsed[1][:annotator] }
+    expect_options("--annotate github_stdout", ["--annotate", "github_stdout"], global: {annotator: :github_stdout})
+    expect_options("-Agithub_stdout", ["-Agithub_stdout"], global: {annotator: :github_stdout})
+    expect_options("--annotate-github-stdout", ["--annotate-github-stdout"], global: {annotator: :github_stdout})
+    expect_options("-G", ["-G"], global: {annotator: :github_stdout})
+    expect_usage_error("--annotate foo_bar", ["--annotate", "foo_bar"], /not recognized/i)
+    expect_usage_error("-Afoo_bar", ["-Afoo_bar"], /not recognized/i)
+  end
+
+  describe "file targeting options" do
+    def self.expect_all_files(desc, arguments, globally:, **tools)
+      tool_args = tools.each_pair.map { |tool, value| [tool, {all_files: value}] }.to_h
+      expect_options(desc, arguments, global: {all_files: globally}, **tool_args)
+    end
+
+    expect_all_files("nothing", [], globally: nil, standardrb: nil, rubocop: nil, rspec: nil)
+    expect_all_files("--all-files", ["--all-files"], globally: true)
+    expect_all_files("-a", ["-a"], globally: true)
+    expect_all_files("--changed-files", ["--changed-files"], globally: false)
+    expect_all_files("-c", ["-c"], globally: false)
+    expect_all_files("--all-files standardrb", ["--all-files", "standardrb"], globally: nil, standardrb: true, rubocop: nil, rspec: nil)
+    expect_all_files("-a -crspec", ["-a", "-crspec"], globally: true, rspec: false, standardrb: nil, rubocop: nil)
+    expect_all_files("-arspec -crubocop", ["-arspec", "-crubocop"], globally: nil, rspec: true, rubocop: false, standardrb: nil)
+  end
+
+  describe "filtering options" do
+    def self.expect_filter_messages(desc, arguments, globally:, **tools)
+      tool_args = tools.each_pair.map { |tool, value| [tool, {filter_messages: value}] }.to_h
+      expect_options(desc, arguments, global: {filter_messages: globally}, **tool_args)
+    end
+
+    expect_filter_messages("nothing", [], globally: nil, standardrb: nil, rubocop: nil, rspec: nil)
+    expect_filter_messages("--filter-messages", ["--filter-messages"], globally: true)
+    expect_filter_messages("-f", ["-f"], globally: true)
+    expect_filter_messages("--unfiltered", ["--unfiltered"], globally: false)
+    expect_filter_messages("-u", ["-u"], globally: false)
+    expect_filter_messages("--filter-messages standardrb", ["--filter-messages", "standardrb"], globally: nil, standardrb: true, rubocop: nil, rspec: nil)
+    expect_filter_messages("-f -urspec", ["-f", "-urspec"], globally: true, rspec: false, standardrb: nil, rubocop: nil)
+    expect_filter_messages("-frspec -urubocop", ["-frspec", "-urubocop"], globally: nil, rspec: true, rubocop: false, standardrb: nil)
+  end
+end
