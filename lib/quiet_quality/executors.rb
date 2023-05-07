8f8da9a11fa6c229e5faec179943ed6b0be72f00
Eric Mueller <nevinera@gmail.com>
2023-05-07 19:08:18 -0400

    add the Executors::Pipeline, for running a single tool end to end
    running the tool, parsing the results, filtering the messages, etc

---
+++ lib/quiet_quality/executors.rb
@@ -0,0 +1,14 @@
+module QuietQuality
+  module Executors
+    Error = Class.new(::QuietQuality::Error)
+  end
+end
+
+glob = File.expand_path("../executors/*.rb", __FILE__)
+Dir.glob(glob).sort.each { |f| require f }
+
+module QuietQuality
+  module Executors
+    AVAILABLE = {}
+  end
+end
