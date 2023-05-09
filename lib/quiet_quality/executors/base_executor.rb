9c3aa226586751b26872176f75029a93e686ea60
Eric Mueller <nevinera@gmail.com>
2023-05-08 22:11:02 -0400

    split the bulk of SerialExecutor into BaseExecutor for reuse
    don't test the abstract class separately though. When we add the
    second implementation, we'll split the bulk of the specs into a shared
    context

---
+++ lib/quiet_quality/executors/base_executor.rb
@@ -0,0 +1,46 @@
+module QuietQuality
+  module Executors
+    class BaseExecutor
+      def initialize(tools:, changed_files: nil)
+        @tools = tools
+        @changed_files = changed_files
+      end
+
+      def execute!
+        fail NoMethodError, "execute! should be implemented by the subclass of BaseExecutor"
+      end
+
+      def outcomes
+        @_outcomes ||= pipelines.map(&:outcome)
+      end
+
+      def messages
+        @_messages ||= Messages.new(pipelines.map(&:messages).map(&:all).reduce(&:+))
+      end
+
+      def any_failure?
+        pipelines.any?(&:failure?)
+      end
+
+      private
+
+      attr_reader :tools, :changed_files
+
+      def pipelines
+        @_pipelines ||= tools.map do |topts|
+          Pipeline.new(tool_options: topts, changed_files: changed_files)
+        end
+      end
+
+      def pipeline_by_tool
+        @_pipeline_by_tool ||= pipelines
+          .map { |p| [p.tool_name, p] }
+          .to_h
+      end
+
+      def pipeline_for(tool)
+        pipeline_by_tool.fetch(tool.to_sym)
+      end
+    end
+  end
+end
