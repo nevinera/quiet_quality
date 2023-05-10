ab4cbe7db1ae43b822261663a4048d83a45f95ee
Eric Mueller <nevinera@gmail.com>
2023-05-10 09:20:28 -0400

    support outputless outcomes more conveniently (entrypoint specs don't care)

--- spec/support/outcome_setup.rb
+++ spec/support/outcome_setup.rb
@@ -1,13 +1,18 @@
 module OutcomeSetup
-  def build_outcome(tool:, output:, logging: nil, failure: false)
-    QuietQuality::Tools::Outcome.new(tool: tool, output: output, logging: logging, failure: failure)
+  def build_outcome(tool:, output: nil, logging: nil, failure: false)
+    QuietQuality::Tools::Outcome.new(
+      tool: tool,
+      output: output || "none",
+      logging: logging,
+      failure: failure
+    )
   end
 
-  def build_success(tool, output, logging = nil)
+  def build_success(tool, output = nil, logging = nil)
     build_outcome(tool: tool, output: output, logging: logging, failure: false)
   end
 
-  def build_failure(tool, output, logging = nil)
+  def build_failure(tool, output = nil, logging = nil)
     build_outcome(tool: tool, output: output, logging: logging, failure: true)
   end
 end
