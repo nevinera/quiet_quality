9d9e9fc7a0a0042ef8850fb5f69e604c225f96e0
Eric Mueller <nevinera@gmail.com>
2023-05-27 23:18:27 -0400

    rewrite Standardrb::Runner in terms of RelevantRunner

--- lib/quiet_quality/tools/standardrb/runner.rb
+++ lib/quiet_quality/tools/standardrb/runner.rb
@@ -1,9 +1,21 @@
 module QuietQuality
   module Tools
     module Standardrb
-      class Runner < Rubocop::Runner
-        def command_name
-          "standardrb"
+      class Runner < RelevantRunner
+        def tool_name
+          :standardrb
+        end
+
+        def no_files_output
+          '{"files": [], "summary": {"offense_count": 0}}'
+        end
+
+        def base_command
+          ["standardrb", "-f", "json"]
+        end
+
+        def relevant_path?(path)
+          path.end_with?(".rb")
         end
       end
     end
