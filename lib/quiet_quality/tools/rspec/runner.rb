5c41a19eb9066e0331f2d8e9993486afb32fc709
Eric Mueller <nevinera@gmail.com>
2023-05-27 23:17:01 -0400

    rewrite Rspec::Runner in terms of RelevantRunner

--- lib/quiet_quality/tools/rspec/runner.rb
+++ lib/quiet_quality/tools/rspec/runner.rb
@@ -1,58 +1,21 @@
 module QuietQuality
   module Tools
     module Rspec
-      class Runner
-        MAX_FILES = 100
-        NO_FILES_OUTPUT = '{"examples": [], "summary": {"failure_count": 0}}'
-
-        def initialize(changed_files: nil, file_filter: nil)
-          @changed_files = changed_files
-          @file_filter = file_filter
+      class Runner < RelevantRunner
+        def tool_name
+          :rspec
         end
 
-        def invoke!
-          @_outcome ||= skip_execution? ? skipped_outcome : performed_outcome
+        def no_files_output
+          '{"examples": [], "summary": {"failure_count": 0}}'
         end
 
-        private
-
-        attr_reader :changed_files, :file_filter
-
-        def skip_execution?
-          changed_files && relevant_files.empty?
+        def base_command
+          ["rspec", "-f", "json"]
         end
 
-        def relevant_files
-          return nil if changed_files.nil?
-          changed_files.paths
-            .select { |path| path.end_with?("_spec.rb") }
-            .select { |path| file_filter.nil? || file_filter.match?(path) }
-        end
-
-        def target_files
-          return [] if changed_files.nil?
-          return [] if relevant_files.length > MAX_FILES
-          relevant_files
-        end
-
-        def command
-          return nil if skip_execution?
-          ["rspec", "-f", "json"] + target_files.sort
-        end
-
-        def skipped_outcome
-          Outcome.new(tool: :rspec, output: NO_FILES_OUTPUT)
-        end
-
-        def performed_outcome
-          out, err, stat = Open3.capture3(*command)
-          if stat.success?
-            Outcome.new(tool: :rspec, output: out, logging: err)
-          elsif stat.exitstatus == 1
-            Outcome.new(tool: :rspec, output: out, logging: err, failure: true)
-          else
-            fail(ExecutionError, "Execution of rspec failed with #{stat.exitstatus}")
-          end
+        def relevant_path?(path)
+          path.end_with?("_spec.rb")
         end
       end
     end
--- spec/quiet_quality/tools/rspec/runner_spec.rb
+++ spec/quiet_quality/tools/rspec/runner_spec.rb
@@ -1,100 +1,58 @@
+require_relative "../runner_examples"
+
 RSpec.describe QuietQuality::Tools::Rspec::Runner do
   let(:changed_files) { nil }
   let(:file_filter) { nil }
   subject(:runner) { described_class.new(changed_files: changed_files, file_filter: file_filter) }
 
-  let(:out) { "fake output" }
-  let(:err) { "fake error" }
-  let(:stat) { instance_double(Process::Status, success?: true, exitstatus: 0) }
-  before { allow(Open3).to receive(:capture3).and_return([out, err, stat]) }
+  it_behaves_like "a functional RelevantRunner subclass", :rspec, {
+    relevant: "foo_spec.rb",
+    irrelevant: "bar.ts",
+    filter: /\.rb/,
+    base_command: ["rspec", "-f", "json"]
+  }
 
-  describe "#invoke!" do
-    subject(:invoke!) { runner.invoke! }
+  describe "#tool_name" do
+    subject(:tool_name) { runner.tool_name }
+    it { is_expected.to eq(:rspec) }
+  end
 
-    context "when the rspec command fails" do
-      let(:stat) { instance_double(Process::Status, success?: false, exitstatus: 14) }
+  describe "#no_files_output" do
+    subject { runner.no_files_output }
+    let(:parsed) { JSON.parse(subject) }
 
-      it "raises an ExecutionError" do
-        expect { invoke! }.to raise_error(QuietQuality::Tools::ExecutionError)
-      end
+    it "contains the expected data" do
+      expect(parsed.dig("examples")).to eq([])
+      expect(parsed.dig("summary", "failure_count")).to eq(0)
+    end
+  end
+
+  describe "#base_command" do
+    subject(:base_command) { runner.base_command }
+    it { is_expected.to eq(["rspec", "-f", "json"]) }
+  end
+
+  describe "#relevant_path?" do
+    subject(:relevant_path?) { runner.relevant_path?(path) }
+
+    context "for a random other file" do
+      let(:path) { "foo/bar.ts" }
+      it { is_expected.to be_falsey }
     end
 
-    context "when the rspec _tests_ fail" do
-      let(:stat) { instance_double(Process::Status, success?: false, exitstatus: 1) }
-      it { is_expected.to eq(build_failure(:rspec, "fake output", "fake error")) }
-
-      it "calls rspec with no targets" do
-        invoke!
-        expect(Open3)
-          .to have_received(:capture3)
-          .with("rspec", "-f", "json")
-      end
+    context "for a non-spec ruby file" do
+      let(:path) { "foo/bar.rb" }
+      it { is_expected.to be_falsey }
     end
 
-    context "when changed_files is nil" do
-      let(:changed_files) { nil }
-      it { is_expected.to eq(build_success(:rspec, "fake output", "fake error")) }
-
-      it "calls rspec with no targets" do
-        invoke!
-        expect(Open3)
-          .to have_received(:capture3)
-          .with("rspec", "-f", "json")
-      end
+    context "for a ruby spec file" do
+      let(:path) { "foo/bar_spec.rb" }
+      it { is_expected.to be_truthy }
     end
 
-    context "when changed_files is empty" do
-      let(:changed_files) { empty_changed_files }
-      it { is_expected.to eq(build_success(:rspec, described_class::NO_FILES_OUTPUT)) }
-
-      it "does not call rspec" do
-        expect(Open3).not_to have_received(:capture3)
-      end
-    end
-
-    context "when changed_files is full" do
-      context "but contains no spec files" do
-        let(:changed_files) { generate_changed_files({"foo_spec.ts" => :all, "bar.rb" => [1, 2], "baz_spec.rb.bak" => [5]}) }
-        it { is_expected.to eq(build_success(:rspec, described_class::NO_FILES_OUTPUT)) }
-
-        it "does not call rspec" do
-          expect(Open3).not_to have_received(:capture3)
-        end
-      end
-
-      context "and contains some spec files" do
-        let(:changed_paths) { ["foo_spec.ts", "bar_spec.rb", "baz_spec.rb.bak", "a/alpha_spec.rb"] }
-        let(:changed_files) { generate_changed_files(changed_paths.map { |p| [p, :all] }.to_h) }
-        it { is_expected.to eq(build_success(:rspec, "fake output", "fake error")) }
-
-        it "calls rspec with the correct targets" do
-          invoke!
-          expect(Open3)
-            .to have_received(:capture3)
-            .with("rspec", "-f", "json", "a/alpha_spec.rb", "bar_spec.rb")
-        end
-
-        context "but some of them are filtered out" do
-          let(:file_filter) { /pha/ }
-          it { is_expected.to eq(build_success(:rspec, "fake output", "fake error")) }
-
-          it "calls rspec with the correct target" do
-            invoke!
-            expect(Open3)
-              .to have_received(:capture3)
-              .with("rspec", "-f", "json", "a/alpha_spec.rb")
-          end
-        end
-
-        context "but all of them are filtered out" do
-          let(:file_filter) { /nobody/ }
-          it { is_expected.to eq(build_success(:rspec, described_class::NO_FILES_OUTPUT)) }
-
-          it "does not call rspec" do
-            expect(Open3).not_to have_received(:capture3)
-          end
-        end
-      end
+    context "for a weirdly named, but non-spec file" do
+      let(:path) { "foo/bar_spec.rb.txt" }
+      it { is_expected.to be_falsey }
     end
   end
 end
